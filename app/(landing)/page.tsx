49c047c27a1d6e98dabb1f1e67664c6a3ce36391
enioaires <enio.aires@hotmail.com>
2024-03-15 12:42:12 -0300

    feat: authentication and landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,68 @@
-import { Button } from "@/components/ui/button";
 import { FC } from "react";
+import Image from "next/image";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home: FC = () => {
-  return <div className="">Landing Page</div>;
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flec-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image alt="hero" src="/hero.svg" fill />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lgÇtext-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Aprenda, treine e masterize novas línguas usando Lingo!
+        </h1>
+        <div>
+          <ClerkLoading>
+            <Loader className="w-5 h-5 animate-spin text-muted-foreground" />
+          </ClerkLoading>
+
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button size="lg" variant="secondary" className="w-full">
+                  Comece Agora
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                afterSignInUrl="/learn"
+                afterSignUpUrl="/learn"
+              >
+                <Button
+                  size="lg"
+                  variant="primeryOutline"
+                  className="w-full mt-2"
+                >
+                  Ja possuo uma conta
+                </Button>
+              </SignInButton>
+            </SignedOut>
+
+            <SignedIn>
+              <Button size="lg" variant="secondary" className="w-full" asChild>
+                <Link href="/learn">Continue Aprendendo</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
